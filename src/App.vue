3da8339602ac23a7dba30bce48c0ec2e672284a4
Dirceu Henrique <dhnasci@hotmail.com>
2020-10-20 19:38:42 -0400

    Login page done

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,9 @@
 <template>
   <v-app>
        <v-container fill-height >
-        <v-layout>  
-            <v-flex>
-                <v-card class="elevation-12">
-                    <v-toolbar color="primary" dark>
-                        <v-toolbar-title>Login</v-toolbar-title>
-                    </v-toolbar>
-                    <v-card-text>
-                        <v-form>
-                            <v-text-field prepend-icon="mdi-email"
-                                name="email"
-                                label="Email"
-                                type="email"> 
-                            </v-text-field>
-                            <v-text-field prepend-icon="mdi-account-lock"
-                                name="password"
-                                label="Senha"
-                                type="password"> 
-                            </v-text-field>
-                            
-                        </v-form>
-                        <v-btn block depressed> Criar conta</v-btn>
-                    </v-card-text>
-                    <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn color="primary" large> Login </v-btn>
-                    </v-card-actions>
-                </v-card>
-            </v-flex> 
-        </v-layout>
+         
+        <router-view/>
+        
     </v-container>
    
   </v-app>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,5 +1,41 @@
 <template>
-        <v-container>
-            <h1>  Login </h1>
-        </v-container>
+       <v-container fill-height >
+        <v-layout justify-center align-center>  
+            <v-flex xs12 sm6 md4 lg3>
+                <v-card class="elevation-12">
+                    <v-toolbar color="primary" dark>
+                        <v-toolbar-title>Login</v-toolbar-title>
+                    </v-toolbar>
+                    <v-card-text>
+                        <v-form>
+                            <v-text-field prepend-icon="mdi-email"
+                                name="email"
+                                label="Email"
+                                type="email"> 
+                            </v-text-field>
+                            <v-text-field prepend-icon="mdi-account-lock"
+                                name="password"
+                                label="Senha"
+                                type="password"> 
+                            </v-text-field>
+                            
+                        </v-form>
+                        <v-btn block depressed> Criar conta</v-btn>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" large> Login </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-flex> 
+        </v-layout>
+    </v-container>
 </template>
+
+<script>
+
+export default {
+  name: 'App'
+};
+</script>
+
