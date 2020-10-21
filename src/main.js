42cecda95aabf43f9c28ccaca8b8442b6d5fb234
Dirceu Henrique <dhnasci@hotmail.com>
2020-10-21 10:59:25 -0400

    Revert "User register"
    This reverts commit 760e6a577141851b8a6d053f0deccd847a41d751.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import vuetify from '@/plugins/vuetify'
 import router from './router'
 import store from './store'
-import vuelidate from '@/plugins/vuelidate'
 
 Vue.config.productionTip = false
 
@@ -11,6 +10,5 @@ new Vue({
   vuetify,
   router,
   store,
-  vuelidate,
   render: h => h(App)
 }).$mount('#app')
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,23 +4,15 @@
             <v-flex xs12 sm6 md4 lg3>
                 <v-card class="elevation-12">
                     <v-toolbar color="primary" dark>
-                        <v-toolbar-title>{{textos.toolbar}}</v-toolbar-title>
+                        <v-toolbar-title>Login</v-toolbar-title>
                     </v-toolbar>
                     <v-card-text>
                         <v-form>
-                            <v-text-field v-if="!isLogin"
-                             prepend-icon="mdi-login"
-                                name="name"
-                                label="Nome"
-                                type="text"> 
-                            </v-text-field>
                             <v-text-field prepend-icon="mdi-email"
                                 name="email"
                                 label="Email"
                                 type="email"
-                                v-model.trim = "$v.user.email.$model"
-                                :error-messages="emailErrors"
-                                :success="!$v.user.email.$invalid"> 
+                               > 
                             </v-text-field>
                             <v-text-field prepend-icon="mdi-account-lock"
                                 name="password"
@@ -29,16 +21,11 @@
                             </v-text-field>
                             
                         </v-form>
-                        <v-btn 
-                            block 
-                            depressed
-                            @click="isLogin = !isLogin"> {{textos.button}}</v-btn>
+                        <v-btn block depressed> Criar conta</v-btn>
                     </v-card-text>
                     <v-card-actions>
                         <v-spacer></v-spacer>
-                        <v-btn 
-                            color="primary" 
-                            large @click="submit()"> {{textos.toolbar}} </v-btn>
+                        <v-btn color="primary" large> Login </v-btn>
                     </v-card-actions>
                 </v-card>
             </v-flex> 
@@ -47,10 +34,9 @@
 </template>
 
 <script>
-import { required, email} from 'vuelidate/lib/validators'
 
-export default {
-  name: 'Login', 
+
+export default {name: 'Login', 
   data: () => ({
         isLogin: true,
         user: {
@@ -64,43 +50,9 @@ export default {
           return this.isLogin 
           ? { toolbar: 'Login', button: 'Criar Conta'}
           : { toolbar: 'Criar Conta', button: 'Já tenho uma conta'}
-      },
-      emailErrors(){
-            const errors = []
-            const email =  this.$v.user.email
-            console.log(email)
-           /* if (!email.$dirty) { return errors }
-            !email.required && errors.push('Email é obrigatório!')
-            !email.email && errors.push('Digite um Email válido!') */
-           return errors
-        }
-  },
-  validations() {
-      const validations = {
-          user: {
-              email: {
-                  required, 
-                  email
-                },
-          }
-        }
-        if (this.isLogin) {
-              return {validations}
-        }
-        return { user:{
-            ...validations.user,
-        }}
-      },
-
-  methods: {
-      log() {
-          console.log('Vuelidate: ', this.$v)
-      }, 
-      submit() {
-          console.log('Usuário: ', this.user)
       }
-
   }
+
 };
 </script>
 
